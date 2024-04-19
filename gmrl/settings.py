5c2c1f14979d7f7aab06d28b81705c4a45c575e7
raveena <raveenalaiju@gmail.com>
2024-04-19 21:14:19 +0530

    some updations done

--- gmrl/settings.py
+++ gmrl/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=['static']
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
